07039900da2d028a12a2fcad1f03744f33324a48
DanielSMReis <danielmaiareis@outlook.com>
2024-03-01 19:49:33 -0300

    criação do clientes html e mudança do clientes>views

--- MECANICA/clientes/views.py
+++ MECANICA/clientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def clientes(request):
-    return HttpResponse('Estou em clientes')
+    return render(request, 'clientes.html')     #por padrao o django ja procura dentro da pasta template
--- MECANICA/mecanica/settings.py
+++ MECANICA/mecanica/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'mecanica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], #onde serao salvas as templates do projeto
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
