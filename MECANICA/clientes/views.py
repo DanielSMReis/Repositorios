f3bf5d5a01d31e3622a89408a25fee6525f00915
DanielSMReis <danielmaiareis@outlook.com>
2024-03-02 01:10:13 -0300

    criando e salvando cliente e carro ok com debug test on views.py, at 1:23:17#1

--- MECANICA/clientes/views.py
+++ MECANICA/clientes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cliente, Carro
 # Create your views here.
 def clientes(request):
     if request.method == "GET":
@@ -13,7 +14,17 @@ def clientes(request):
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
 
-
-
+        #importando a classe cliente migrada para banco de dados(primeiro precisa ter cliente para ter um carro, lembre-se)
+        cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+        cliente.save()
+        # X = list(zip(carros, placas, anos)) #concatenando e correlacionando cada carro com seus respectivos dados com a função zip e transformando em lista para retirar os numeros de memoria
+        for carro, placa, ano in zip(carros, placas, anos):
+            car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
+            car.save()
 
         return HttpResponse('teste')
