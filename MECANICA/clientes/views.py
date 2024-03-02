66db1ccfa029963cc2d61d9cb9b25006d0a9fdd8
DanielSMReis <danielmaiareis@outlook.com>
2024-03-02 09:45:34 -0300

    terminado #1 projeto funcionando , salvando informaçoes caso form cliente seja cadastrado com dados invalidos

--- MECANICA/clientes/views.py
+++ MECANICA/clientes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Cliente, Carro
+import re
 # Create your views here.
 def clientes(request):
     if request.method == "GET":
@@ -13,7 +14,15 @@ def clientes(request):
         carros = request.POST.getlist('carro')
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
+        #testando se cliente ja existe
+        cliente = Cliente.objects.filter(cpf=cpf)
 
+        if cliente.exists():
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'email':email, 'carros': zip(carros, placas, anos)})
+        
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email): 
+            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'cpf':cpf, 'carros': zip(carros, placas, anos)})
+        #AT 1:28:40#1
         #importando a classe cliente migrada para banco de dados(primeiro precisa ter cliente para ter um carro, lembre-se)
         cliente = Cliente(
             nome = nome,
