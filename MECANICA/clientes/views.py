930369473925ec776e00a5dcba0b60912541bdd3
DanielSMReis <danielmaiareis@outlook.com>
2024-03-07 21:46:17 -0300

    utilizando CSRFToken, tudo funcioando at29:07 #2

--- MECANICA/clientes/views.py
+++ MECANICA/clientes/views.py
@@ -15,16 +15,20 @@ def clientes(request):
         carros = request.POST.getlist('carro')
         placas = request.POST.getlist('placa')
         anos = request.POST.getlist('ano')
+
+
         #testando se cliente ja existe
         cliente = Cliente.objects.filter(cpf=cpf)
 
+
         if cliente.exists():
-            return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'email':email, 'carros': zip(carros, placas, anos)})
+            return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email':email, 'carros': zip(carros, placas, anos)})
         
         if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email): 
             return render(request, 'clientes.html', {'nome':nome, 'sobrenome':sobrenome, 'cpf':cpf, 'carros': zip(carros, placas, anos)})
         #AT 1:28:40#1
         #importando a classe cliente migrada para banco de dados(primeiro precisa ter cliente para ter um carro, lembre-se)
+        
         cliente = Cliente(
             nome = nome,
             sobrenome = sobrenome,
@@ -32,7 +36,7 @@ def clientes(request):
             cpf = cpf
         )
         cliente.save()
-        # X = list(zip(carros, placas, anos)) #concatenando e correlacionando cada carro com seus respectivos dados com a função zip e transformando em lista para retirar os numeros de memoria
+        
         for carro, placa, ano in zip(carros, placas, anos):
             car = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
             car.save()
--- MECANICA/templates/static/clientes/js/clientes.js
+++ MECANICA/templates/static/clientes/js/clientes.js
@@ -7,6 +7,7 @@ function add_carro(){
     container.innerHTML += html
 }
 
+
 function exibir_form(tipo){
 
     add_cliente = document.getElementById('adicionar-cliente')
@@ -22,4 +23,33 @@ function exibir_form(tipo){
 
     }
     
+}
+
+
+function dados_clientes(){
+    cliente = document.getElementById('cliente-select')
+
+    //utilizando FETCH é uma forma de fazer requisiçoes para um endpoint em um determinado backend
+    //precisa passar o dominio para o qual sera passado a requisição, depois os dados(method)
+    //quando precisamos passar uma req HTTP precisamos enviar dois tipos de dados para o backend que é o cabeçalho e o corpo da req
+    //as funçoes do django exigem que seja passada no corpo da req o csrftoken, que esta sendo gerada em clientes.html linha 22 (esse token gera um value unico que pode ser usado para referenciar a função)
+    csrf_token = document.querySelector('[name=csrfmiddlewaretoken]') //obtendo o token
+    id_cliente = cliente.value
+
+    data = new FornData()
+    data.append('id_cliente', id_cliente)
+
+    fetch("/clientes/atualiza_cliente/",{
+        method: "POST",
+        headers: {
+            'X-CSRFToken': csrf_token,  //garantimos que nao havera falsificação de formulários
+        },
+        body: data
+    }).then(function(result){//depois de enviar a req, o servidor vai mandar uma resposta, por isso isamos o .then para aguardar essa resposta, depois esta é passada para function e convertida em um json
+        return result.json()
+
+    }).then(function(data){
+        console.log('testee')
+    })
+
 }
