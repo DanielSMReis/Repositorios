168e02339de9465ff17d27e454ffbb51c7602576
DanielSMReis <danielmaiareis@outlook.com>
2024-07-21 13:01:19 -0300

    feita implementação da geração do arquivo PDF e edicao no layout do mesmo, all clear at0000 #9

--- MECANICA/servicos/views.py
+++ MECANICA/servicos/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 #importando o formulario local "." criado no backend
 from .Forms import FormServico
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from .models import Servico
 from fpdf import FPDF
+from io import BytesIO
 # Create your views here.
 def novo_servico(request):
     if request.method == "GET":
@@ -39,17 +40,37 @@ def gerar_os(request, identificador):
     #é necessário criar uma pagina, setando fonte e outros parametros para a criacao do arquivo
     pdf.add_page()
     #nome da fonte, formato(italico,negrito..), tamanho
-    pdf.set_font('Arial', 'B', 16)
+    pdf.set_font('Arial', 'B', 12)
     #criando uma tablea
     pdf.set_fill_color(240,240,240)
     #para pdf_cell(altura,largura,titulo,borda(0/1), pular linha(0/1),alinhamento(L/C/R),cor de fundo(0/1))
-    pdf.cell(35, 10, 'Cliente:', 1, 0, 'L', 1)
+    pdf.cell(45, 10, 'Cliente:', 1, 0, 'L', 1)
     pdf.cell(0, 10, f'{servico.cliente.nome}', 1, 1, 'L', 1)
-    pdf.cell(35, 10, 'Manutenções:', 1, 0, 'L',1)
+    pdf.cell(45, 10, 'Manutenções:', 1, 0, 'L',1)
 
-    for manutencao in servico.categoria_manutencao.all():
+    enumerar_categorias = servico.categoria_manutencao.all()
+    for i, manutencao in enumerate(enumerar_categorias):
         pdf.cell(0, 10, f'- {manutencao.get_titulo_display()}', 1, 1, 'L', 1)
-    pdf.output('os.pdf')
+        if not i == len(enumerar_categorias) -1:
+            pdf.cell(35,10, '',0,0)
+    
 
+    pdf.cell(45,10, 'Data de início:', 1, 0,'L',1)
+    pdf.cell(0,10, f'{servico.data_inicio}', 1,1,'L',1)
+    pdf.cell(45,10, 'Data de entrega:', 1, 0,'L',1)
+    pdf.cell(0,10, f'{servico.data_entrega}', 1,1,'L',1)
+    pdf.cell(35,10, 'Protocolo:', 1, 0,'L',1)
+    pdf.cell(0,10, f'{servico.protocolo}', 1,1,'L',1)
+    pdf.cell(35,10, 'Preço total:', 1, 0,'L',1)
+    pdf.cell(0,10, f'{servico.preco_total()}', 1,1,'L',1)
+    
+    #Salvando o arquivo PDF em memoria para que seja exibido para os demais usuarios do sistema.
+    #Usando o parametro "dest=S" definimos que estaremos salvando em memoria.
+    #como a função nao devolve uma str, precisa codificar usando o encode
+    pdf_content = pdf.output(dest='S').encode('latin1')
 
-    return HttpResponse(identificador)
+    #como o django nao consegue retornar o arquivo em response http, é necessário fazer com que ele leia de fato o arquivo usando o FileResponse. 
+    #para isto, é necessario passar o arquivo e os bytes dele, usando o bytesIO da biblioteca IO do python
+    pdf_bytes = BytesIO(pdf_content)
+    #Para salvar automaticamente o PDF, basta por o parametro: "as_attachment=True" antes de filename.
+    return FileResponse(pdf_bytes, filename=f"os-{servico.protocolo}.pdf")
